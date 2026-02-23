9d2c1a0780f7c1ed3d5e70a29e26a125bc626b47
deonugroho <deonugroz888@gmail.com>
2026-02-23 17:02:45 +0700

    mengubah format luas/l menjadi tinggi/t

---
+++ hitungLuas.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int p, t, luas;
+
+    //langkah 2
+    cout << "Masukkan panjang: ";
+    cin >> p;
+
+    //langkah 3
+    cout << "Masukkan tinggi: ";
+    cin >> t;
+
+    //langkah 4
+    luas = p*t;
+
+    //langkah 5
+    cout << "Hasilnya= " << luas;
+}
